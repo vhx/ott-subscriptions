b4dc564c1b59aa59ba3f440766044de7b40134d2
Matthew Li <matthewli2491@gmail.com>
2018-12-10 11:29:05 -0500

    got react running

--- app/javascript/pages/home/index.jsx
+++ app/javascript/pages/home/index.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import App from '../../components/App'
 
-const Index = props => (
-  <div>Hello {props.name}!</div>
-)
+// const Index = props => (
+//   <div>Hello {props.name}!</div>
+// )
 
-Index.defaultProps = {
-  name: ''
-}
+// Index.defaultProps = {
+//   name: ''
+// }
 
-Index.propTypes = {
-  name: PropTypes.string
-}
+// Index.propTypes = {
+//   name: PropTypes.string
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Index name="React" />,
+    <App />,
     document.body.appendChild(document.createElement('div')),
   )
 })
