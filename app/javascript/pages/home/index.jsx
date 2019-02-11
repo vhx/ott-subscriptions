1f493c4be7913ce0fb5fcfd4dbf6b18bfbe0c286
srscalia <shelby.scalia@gmail.com>
2019-02-11 17:25:53 -0500

    render emails to page

--- app/javascript/pages/home/index.jsx
+++ app/javascript/pages/home/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 import Subscriptions from './subscriptions';
 
-const Index = props => (
+const Index = () => (
   <Subscriptions/>
 )
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Index name="React" />,
+    <Index/>,
     document.body.appendChild(document.createElement('div')),
   )
 })
--- app/javascript/pages/home/subscriptions.jsx
+++ app/javascript/pages/home/subscriptions.jsx
@@ -1,28 +1,39 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
+import React, {
+  Component
+} from 'react';
 
-class Subscriptions extends Component  {
+class Subscriptions extends Component {
   state = {
-    customers: {}
+    subscriptions: {}
+  }
+
+  renderSubscriptions = () => {
+    if (this.state.subscriptions.length > 1) {
+      return this.state.subscriptions.map(s=><div key={s.id}>{s.customer.email}</div>)
+    }
   }
 
   render() {
-      return (
-        <div>hi</div>
-      )
-    }
+    return ( <div> {
+      this.renderSubscriptions()
+    } </div>)
+  }
 
-  componentDidMount(){
+  componentDidMount() {
     this.fetchCustomers()
   }
 
   fetchCustomers = () => {
     fetch('http:localhost:3000/subscriptions')
-    .then(r=>r.json())
-    .then(json=> console.log(json[0]))
+      .then(r => r.json())
+      .then(json => {
+        this.setState({
+          subscriptions: json
+        })
+      })
   }
 
 }
 
 
-export default Subscriptions
+  export default Subscriptions
