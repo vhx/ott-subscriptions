62e28119a9c8f6c06d1ae9644f7c97ee2246498b
srscalia <shelby.scalia@gmail.com>
2019-02-13 15:28:15 -0500

    complete pagination for desktop

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -15,7 +15,7 @@ class ApiController < ApplicationController
     {
       current_page: collection.current_page,
       next_page: collection.next_page,
-      prev_page: collection.prev_page, # use collection.previous_page when using will_paginate
+      prev_page: collection.prev_page,
       total_pages: collection.total_pages,
       total_count: collection.total_count
     }
--- app/javascript/pages/home/pagination.jsx
+++ app/javascript/pages/home/pagination.jsx
@@ -3,10 +3,23 @@ import { connect } from 'react-redux'
 
 class Pagination extends Component {
 
+  handleClick = (propertyName) => {
+    if (propertyName === 'next' && this.props.current_page < this.props.total_pages) {
+      fetch(`http:localhost:3000/subscriptions?page=`+`${this.props.next_page}`)
+      .then(r=>r.json())
+      .then(json=>this.props.updateSubscriptions(json))
+
+    } else if (propertyName=== 'previous' && this.props.current_page > 1) {
+      fetch(`http:localhost:3000/subscriptions?page=`+`${this.props.prev_page}`)
+      .then(r=>r.json())
+      .then(json=>this.props.updateSubscriptions(json))
+    }
+  }
+
   render() {
     return(
       <div className="text-center">
-         &lt;&lt; {this.props.current_page} of {this.props.total_pages}  &gt;&gt;
+         <span onClick={()=> this.handleClick("previous")} className="arrows">&lt;&lt;</span> {this.props.current_page} of {this.props.total_pages}  <span onClick={()=> this.handleClick("next")} className="arrows">&gt;&gt;</span>
       </div>
     )
   }
@@ -16,9 +29,21 @@ class Pagination extends Component {
 function mapStateToProps(state){
   return {
     current_page: state.current_page,
-    total_pages: state.total_pages
+    total_pages: state.total_pages,
+    next_page: state.next_page,
+    prev_page: state.prev_page,
+    current_page: state.current_page
+  }
+}
+
+function mapDispatchToState(dispatch){
+  return {
+    updateSubscriptions: (json)=> dispatch({
+      type: "UPDATE_SUBSCRIPTIONS",
+      payload: json
+    })
   }
 }
 
 
-export default connect(mapStateToProps)(Pagination)
+export default connect(mapStateToProps, mapDispatchToState)(Pagination)
