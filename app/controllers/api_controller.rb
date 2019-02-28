6183f751aed82f17e1cde589a095dd5a51a77f80
Andrew Viteri <aryn.viteri@gmail.com>
2019-02-28 12:30:42 -0500

    Implement api.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,13 +5,48 @@ class ApiController < ApplicationController
   	WillPaginate.per_page = @MAX_PAGE_RESULTS
   end
 
+
   # GET endpoint, returns subscriptions by query. 
   def subscriptions
-    @subscriptions = []
+  	# Data from GET request
+    query = params["query"]
+    page = params["page"].to_i
+
+    if page < 1 || !page
+      page = 1
+    end
+
+    return search_subscriptions(query, page)
   end
 
+
   # Returns subscriptions by query and page number.
-  def search_subscriptions
+  def search_subscriptions(query, page)
     # EXTRA CREDIT: Implement searching for email, product, and subscription type
+    if !query
+      @subscriptions = Subscription.page(page)
+    else
+      customer_ids = Customer.where("lower(email) LIKE ?", "%#{query.downcase}%").map {|c| c.id} # Locate customers
+      product_ids = Product.where("lower(name) LIKE ?", "%#{query.downcase}%").map {|prdct| prdct.id} # Locate products
+      # Find subscriptions by email, product, or subscription type #
+      @subscriptions = Subscription.where(customer_id: customer_ids)
+    							.or(Subscription.where(product_id: product_ids))
+    							.or(Subscription.where("lower(billing_type) LIKE ?", "%#{query.downcase}%"))
+    							.page(page)
+    end
+
+    res = {
+      page: page,
+      total_pages: @subscriptions.total_pages,
+      subscriptions: @subscriptions.order(subscribed_at: :desc).map {|s| {
+      	customer_email: Customer.where(id: s.customer_id).first.email,
+      	product_name: Product.where(id: s.product_id).first.name,
+      	billing_type: s.billing_type,
+      	subscribed_at: s.subscribed_at
+      }}
+    }
+    render :json => res
   end
+
+
 end
