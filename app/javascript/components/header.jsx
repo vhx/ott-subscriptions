c77f13355ce8171c57af6bdd9b6de63b76e7dcd7
Andrew Viteri <aryn.viteri@gmail.com>
2019-02-28 13:57:37 -0500

    Outline SearchBar component. Add FontAwesome dependency (npm)

--- app/javascript/components/header.jsx
+++ app/javascript/components/header.jsx
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 
 class Header extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			img: props.img,
-			title: props.title,
-			desc: props.desc
-		};
-	}
+  constructor(props) {
+	super(props);
+	this.state = {
+ 	  img: props.img,
+	  title: props.title,
+	  desc: props.desc
+	};
+  }
 
-	render() {
-		return(
-			<div className="header">
-				<div className="img-container">
-					<img src={this.state.img} />
-				</div>
-				<div className="text-container">
-					<div id="title">{this.state.title}</div>
-					<div id="desc">{this.state.desc}</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+	return(
+	  <div className="header">
+		<div className="img-container">
+		  <img src={this.state.img} />
+		</div>
+		<div className="text-container">
+		  <div id="title">{this.state.title}</div>
+		  <div id="desc">{this.state.desc}</div>
+		</div>
+	  </div>
+	);
+  }
 
 }
 
---
+++ app/javascript/components/search_bar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+  constructor(props) {
+  	super(props);
+  	this.state = {
+  		value: ''
+  	};
+  }
+
+  handleChange(e) {
+  	const current_input = e.target.value;
+  	this.setState({
+  		value: current_input
+  	});
+  }
+
+  handleKeyPress(e) {
+  	if (e.key === "Enter") {
+  		return this.submit();
+  	}
+  }
+
+  /**
+   * Passes data in search bar to props submit function.
+   */
+  submit() {
+  	const val = this.state.value;
+  	return this.props.submit(val);
+  }
+
+  render() {
+  	return(
+  	  <div className="search-bar">
+  	  	<input type="text" name="search-input" placeholder={this.props.placeholder} onChange={this.handleChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this)}/>
+  	  	<button className="search-button" onClick={this.submit.bind(this)}>
+  	  		
+  	  	</button>
+  	  </div>
+  	);
+  }
+
+}
